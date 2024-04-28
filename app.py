8d60d0d63415145698187fe3061a30b7fc05db2c
Shubham Sawant <shubham.sawant42@outlook.com>
2024-04-28 20:36:24 +0530

    Added home page code for flask Ecomm website

--- app.py
+++ app.py
@@ -12,7 +12,48 @@ app.secret_key = secret_key
 
 @app.route('/')
 def main():
-    return render_template('index.html')
+    # Get a cursor from the database connection
+    cursor = mysql.connection.cursor()
+    # Retrieve data from the database
+    cursor.execute("SELECT * FROM banners WHERE status = '1'")
+    banners = cursor.fetchall()
+
+    # Define SQL queries
+    categories_query = "SELECT * FROM categories WHERE main_cat = '0' LIMIT 7;"
+    currency_query = "SELECT * FROM rates WHERE status = '1';"
+
+    # Check if user is logged in and set the user email or session ID accordingly
+    if 'user_id' in session:
+        user_email = session['email']
+        user_cart_query = f"SELECT * FROM cart WHERE user_email = '{user_email}';"
+    else:
+        session_id = session.get('session_id')
+        if session_id is not None:
+            user_cart_query = f"SELECT * FROM cart WHERE session_id = '{session_id}';"
+        else:
+            user_cart_query = ""
+
+    # Execute SQL queries
+    cursor.execute(categories_query)
+    categories = cursor.fetchall()
+
+    cursor.execute(currency_query)
+    currencyall = cursor.fetchall()
+
+    if user_cart_query:
+        cursor.execute(user_cart_query)
+        user_cart = cursor.fetchall()
+    else:
+        user_cart = []
+
+    # Close the cursor and database connection
+    cursor.close()
+
+
+
+    # Render the index.html template with the retrieved data
+    return render_template('index.html', banners=banners, categories=categories, currencyall=currencyall, user_cart=user_cart)
+
 
 
 @app.route('/products')
@@ -39,6 +80,12 @@ def logout():
     session.pop('user_id', None)
     # Redirect to the home page or any other desired page after logout
     return redirect(url_for('main'))
+
+
+@app.route('/templates/<int:id>')
+def templates(id):
+    # Your route logic goes here
+    return f'Templates for category ID {id}'
 
 
 if __name__ == '__main__':
